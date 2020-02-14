58b81dc84b9dee6b612baac24faad0a8e43c4249
Bratislav <37560533+BratislavStojanovic@users.noreply.github.com>
2020-02-14 16:54:25 +0100

    Add files via upload

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Jumbotron = () => {
+  return (
+    <div>
+      <div className='jumbotron text-center'>
+        <h3>React Todo</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Todo = ({ todo, index, markTodo, deleteTodo }) => {
+  return (
+    <div className='col-4'>
+      <div className='card text-center'>
+        <div className='card-header'>
+          <h3>Todo: {index + 1}</h3>
+        </div>
+        <div className='card-body'>
+          <h3 className={todo.done ? 'complete' : 'incomplete'}>{todo.msg}</h3>
+        </div>
+        <div className='card-footer'>
+          <button
+            onClick={() => {
+              deleteTodo(index);
+            }}
+            className='btn btn-danger float-left'
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => {
+              markTodo(index);
+            }}
+            className='btn btn-warning float-right'
+          >
+            Mark
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
